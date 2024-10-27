c4f13f4feb0e1772bb4976207745949b0eabde60
raycastillo3 <60750068+raycastillo3@users.noreply.github.com>
2024-10-27 19:24:23 -0400

    last commit

--- index.js
+++ index.js
@@ -19,24 +19,26 @@
 
 //Flat the array
 const flatArray = (arr) => {
-    if (arr.length == 1) return arr;
+    if (arr.length == 0) return [];
     if (Array.isArray(arr[0])){
-        return [...arr[0], flatArray(arr.slice(1))];
+        return [...flatArray(arr[0]), ...flatArray(arr.slice(1))];
     } else {
-        return [arr[0], flatArray(arr.slice(1))]
+        return [arr[0], ...flatArray(arr.slice(1))];
     }
 }
-// trampoline: 
+//trampoline
 const trampoline = (f, ...args) => {
     let result = f(...args);
-    while (typeof result === "function"){
-        result = result()
+    while (typeof result === "function") {
+      result = result();
     }
     return result;
-}             
-const inputArr = [[0,1,2,3], [7,8,9,10]];
+  }       
+const inputArr = [[0,1,2,3],[4,5,6], [7,8,9,10]];
 // const inputArr2 = [0, 1, [2, [3, [4, 5]]]];
 // [0, 1, 2, 3, 4, 5];
-console.log(trampoline(flatArray(inputArr)));
-// console.log(flatArray(inputArr2));
+console.log(trampoline(flatArray, inputArr));
+// console.log(trampoline(flatArray, inputArr2));
 
+
+console.log();
